ffaf0e9117541f90346077bde439d52f75c7916e
JeffMachado <jeffthedev@icloud.com>
2025-12-15 08:19:43 -0300

    Initial Commit

---
+++ proverbiandoTests/proverbiandoTests.swift
@@ -0,0 +1,17 @@
+//
+//  proverbiandoTests.swift
+//  proverbiandoTests
+//
+//  Created by Jefferson Machado on 15/12/25.
+//
+
+import Testing
+@testable import proverbiando
+
+struct proverbiandoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
